45fa986371b1ff5a0e35537d5d940e8ea46d0f7d
easymean <m2162003@naver.com>
2025-11-16 20:07:16 +0900

    fix: singlton 구현

--- src/grimpan.ts
+++ src/grimpan.ts
@@ -1,15 +1,17 @@
 class Grimpan {
-  private static instance: HTMLCanvasElement | null = null;
+  static instance: Grimpan;
 
   private constructor(canvas: HTMLCanvasElement | null) {
-    if (!canvas) {
+    if (!canvas || !(canvas instanceof HTMLCanvasElement)) {
       throw new Error('Canvas is not found');
     }
   } // constructor 는 외부에서 호출할 수 없도록 private 로 선언
 
   static getInstance() {
     if (!this.instance) {
-      this.instance = document.getElementById('canvas') as HTMLCanvasElement;
+      this.instance = new Grimpan(
+        document.getElementById('canvas') as HTMLCanvasElement
+      );
     }
     return this.instance;
   }
